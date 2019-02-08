21fe8228e7ff20f99a1159b187dd2227a5eff407
eemj <jamiefarrugia@msn.com>
2019-02-08 10:59:21 +0100

    Run eslint with fix arguments

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const path = require("path");
-const chalk = require("chalk");
-const inquirer = require("inquirer");
-const styleGuides = require("./styles");
-const { execSync, spawn } = require("child_process");
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const styleGuides = require('./styles');
+const { execSync, spawn } = require('child_process');
 
 const { bold, cyan: highlight } = chalk;
-const dash = chalk.green("-");
+const dash = chalk.green('-');
 
 // Prompt
 const promptUser = () =>
@@ -16,18 +16,18 @@ const promptUser = () =>
     inquirer
       .prompt([
         {
-          type: "list",
-          name: "style",
-          message: "Which style guide would you want to follow?",
+          type: 'list',
+          name: 'style',
+          message: 'Which style guide would you want to follow?',
           choices: styleGuides.map(style => ({
             value: style.value,
             name: style.name
           }))
         },
         {
-          type: "confirm",
-          name: "react",
-          message: "Are you going to use React?"
+          type: 'confirm',
+          name: 'react',
+          message: 'Are you going to use React?'
         }
       ])
       .then(answers => {
@@ -49,27 +49,27 @@ const promptUser = () =>
 promptUser()
   .then(configs => {
     // If we don't find a package.json file we'll create one.
-    if (!fs.existsSync(path.join(process.cwd(), "package.json"))) {
+    if (!fs.existsSync(path.join(process.cwd(), 'package.json'))) {
       console.log(
         [
           dash,
-          bold("Creating a"),
-          highlight("package.json"),
-          bold("file ...")
-        ].join(" ")
+          bold('Creating a'),
+          highlight('package.json'),
+          bold('file ...')
+        ].join(' ')
       );
 
-      execSync("npm init -y");
+      execSync('npm init -y');
     }
 
     // Fetching peerDependencies for our configs.
     console.log(
       [
         dash,
-        bold("Fetching infos for"),
-        configs.map(config => highlight(config)).join(bold(", ")),
-        bold("...")
-      ].join(" ")
+        bold('Fetching infos for'),
+        configs.map(config => highlight(config)).join(bold(', ')),
+        bold('...')
+      ].join(' ')
     );
 
     const peers = new Map();
@@ -81,7 +81,7 @@ promptUser()
       for (const dependency in peerDependencies) {
         if (!dependency) continue;
         let version = peerDependencies[dependency];
-        if (version.indexOf("||")) version = version.split(" || ").pop();
+        if (version.indexOf('||')) version = version.split(' || ').pop();
         if (!peers.has(dependency)) peers.set(dependency, version);
       }
     }
@@ -89,52 +89,52 @@ promptUser()
     const packages = [...peers]
       .map(([dependency, version]) => ({ dependency, version }))
       .concat(
-        configs.map(config => ({ dependency: config, version: "latest" }))
+        configs.map(config => ({ dependency: config, version: 'latest' }))
       );
 
     // Installing the packages we recieved above.
     console.log(
       [
         dash,
-        bold("Installing"),
-        packages.map(pkg => highlight(pkg.dependency)).join(bold(", ")),
-        bold("...")
-      ].join(" ")
+        bold('Installing'),
+        packages.map(pkg => highlight(pkg.dependency)).join(bold(', ')),
+        bold('...')
+      ].join(' ')
     );
 
     const packageInstall = spawn(
-      "npm",
+      'npm',
       [
-        "install",
-        "-D",
+        'install',
+        '-D',
         ...packages.map(pkg => `${pkg.dependency}@${pkg.version}`)
       ],
       {
-        stdio: "inherit"
+        stdio: 'inherit'
       }
     );
 
-    packageInstall.on("close", () => {
+    packageInstall.on('close', () => {
       // Creating a JSON format eslint configuration.
-      console.log(dash, bold("Creating configuration ..."));
+      console.log(dash, bold('Creating configuration ...'));
 
       fs.writeFileSync(
-        path.join(process.cwd(), ".eslintrc.json"),
+        path.join(process.cwd(), '.eslintrc.json'),
         JSON.stringify(
           {
             extends:
               configs.length === 1
                 ? configs[0].match(/eslint-config-(.*?)$/)[1]
                 : [
-                    ...configs.map(
-                      config => config.match(/eslint-config-(.*?)$/)[1]
-                    )
-                  ]
+                  ...configs.map(
+                    config => config.match(/eslint-config-(.*?)$/)[1]
+                  )
+                ]
           },
           null,
           2
         ),
-        "utf8"
+        'utf8'
       );
     });
   })
